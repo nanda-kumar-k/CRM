239711bf3710ce579b5f1b339d651582cefdd291
Krishna Teja <2000031884@kluniversity.in>
2022-07-01 19:27:53 +0530

    Krishna2

--- src/main/java/com/model/EmployeeManager.java
+++ src/main/java/com/model/EmployeeManager.java
@@ -1,16 +1,15 @@
 package com.model;
 
 
-import java.util.List;
-
 import javax.ejb.Stateless;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
 
+
+import com.entity.Company;
 import com.entity.Employee;
 
 @Stateless
@@ -66,4 +65,53 @@ public class EmployeeManager implements EmployeeRemote{
 		return 0;
 	}
 
+	@Override
+	public int cwriteData(String username,String password,String email,String phone){
+		try {
+			Company E=new Company();
+			E.setEmail(email);
+			E.setName(username);
+			E.setPassword(password);
+			E.setPhone(phone);
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("CRM");
+			EntityManager em = emf.createEntityManager();
+			em.getTransaction().begin();
+			em.persist(E);
+			em.getTransaction().commit();
+			em.close();
+			emf.close();
+			return 1;
+	       }
+		catch(Exception e) {
+			
+		}
+		return 0;
+	}
+	
+	@Override
+	public int cloginData(String username,String password) {
+		
+		try {
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("CRM");
+			EntityManager em = emf.createEntityManager();
+			em.getTransaction().begin();
+			Company E = em.find(Company.class, username);
+			if(E.getPassword().equals(password))
+				return 1;
+			//Query qry = em.createQuery("select e from example e where e.username=?1 and e.password=?2");
+			//qry.setParameter(1,username);
+			//qry.setParameter(2, password);
+			//@SuppressWarnings("unchecked")
+			//List<Employee> E=qry.getResultList();
+			//if(E!=null && e!=null)
+			em.getTransaction().commit();
+			em.close();
+			emf.close();
+		}
+		catch(Exception e) {
+			
+		}
+		return 0;
+	}
+	
 }
--- src/main/java/com/model/EmployeeRemote.java
+++ src/main/java/com/model/EmployeeRemote.java
@@ -4,4 +4,6 @@ public interface EmployeeRemote {
 
 	public int writeData(String username,String password,String email,String phone);
 	public int loginData(String username,String password);
+	public int cwriteData(String username,String password,String email,String phone);
+	public int cloginData(String username,String password);
 }
