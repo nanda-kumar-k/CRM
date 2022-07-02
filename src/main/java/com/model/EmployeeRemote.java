a54c46bac06caeca2658db110372d4d16d0170f0
Krishna Teja <2000031884@kluniversity.in>
2022-07-02 16:15:53 +0530

    JSF Integrated with JPA - KT

--- src/main/java/com/model/EmployeeRemote.java
+++ src/main/java/com/model/EmployeeRemote.java
@@ -2,8 +2,8 @@ package com.model;
 
 public interface EmployeeRemote {
 
-	public int writeData(String username,String password,String email,String phone);
+	public int writeData(String username,String password,String email,String phone,String name,String edu,String jobRole,String location);
 	public int loginData(String username,String password);
-	public int cwriteData(String username,String password,String email,String phone);
+	public int cwriteData(String username,String password,String email,String name,String location);
 	public int cloginData(String username,String password);
 }
